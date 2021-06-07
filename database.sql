93039828402417ed12b217bf3b1ee3aa8007e210
jamesiweobi <60060852+jamesiweobi@users.noreply.github.com>
2021-06-07 01:10:39 +0100

    removed comments in the database.sql file

--- database.sql
+++ database.sql
@@ -1,12 +1,5 @@
-CREATE DATABASE storeuser;
-
-
---\c playerdb;   
-\c playerdb;
-
 CREATE TABLE users(
   userid SERIAL PRIMARY KEY, 
   username VARCHAR(100) NOT NULL,
-  description VARCHAR(300) NOT NULL,
+  description VARCHAR(300) NOT NULL
 )
-
